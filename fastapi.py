984b698abe59c806514f7aa6ce04fa8aa8c9c89a
Pinakesh <pinakesh.chatto2016@gmail.com>
2025-10-21 14:18:32 +0530

    modified backend

--- fastapi.py
+++ fastapi.py
@@ -9,11 +9,13 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins for production
+    allow_origins=["http://localhost:5173", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 
 # Load ONNX model at startup
 try:
